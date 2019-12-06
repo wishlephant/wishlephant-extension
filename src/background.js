9371a5b829927668f5aa16d09eee75b85c6ff40f
Bodo Tasche <bodo@wannawork.de>
2019-12-06 14:22:34 +0100

    added url check

--- src/background.js
+++ src/background.js
@@ -3,10 +3,18 @@ var browser = browser || chrome;
 function openPage() {
   browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var tab = tabs[0];
-    browser.tabs.create({
-      url:
-        "https://wishlephant.com/add_entry?url=" + encodeURIComponent(tab.url)
-    });
+    try {
+      var url = new URL(tab.url);
+      if (url.protocol === "http:" || url.protocol === "https:") {
+        browser.tabs.create({
+          url:
+            "https://wishlephant.com/add_entry?url=" +
+            encodeURIComponent(tab.url)
+        });
+      }
+    } catch (e) {
+      console.log("not a valid url");
+    }
   });
 }
 
